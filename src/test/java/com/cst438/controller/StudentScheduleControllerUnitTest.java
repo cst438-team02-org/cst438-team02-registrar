966d1e106f8800cc92dd2a9f6f769cea3706b44e
Dakota Fouch <dfouch@csumb.edu>
2025-11-21 19:47:06 -0800

    altered student schedule controller unit tests to not depend on predefined data in dataset.
    Unit tests now create terms, courses, & sections per unit test, no longer relying on specific data in dataset.

--- src/test/java/com/cst438/controller/StudentScheduleControllerUnitTest.java
+++ src/test/java/com/cst438/controller/StudentScheduleControllerUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.sql.Date;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +27,12 @@ public class StudentScheduleControllerUnitTest {
     private WebTestClient client ;
     @Autowired
     private EnrollmentRepository enrollmentRepository;
+    @Autowired
+    private SectionRepository sectionRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private TermRepository termRepository;
 
     // default behavior for a Mock bean
     // return 0 or null for a method that returns a value
@@ -37,17 +44,44 @@ public class StudentScheduleControllerUnitTest {
     // test student login, add course, & delete course from student's schedule
     @Test
     public void studentScheduleCreateDelete() throws Exception {
+
+        // reset databases used in this test
+        enrollmentRepository.deleteAll();
+        sectionRepository.deleteAll();
+        courseRepository.deleteAll();
+        termRepository.deleteAll();
+
         /* Student Login */
         String studentEmail = "sam@csumb.edu";
         String password = "sam2025";
 
         String jwt = loginUser(studentEmail, password);
 
-        int sectionNo = 1; // default section already in database
+        /* Generate Term */
+        Term t = createTerm(1,
+                2026,
+                "Spring",
+                "2025-11-01",
+                "2026-04-30",
+                "2026-04-30",
+                "2026-01-15",
+                "2026-05-17");
+
+        /* Generate Course */
+        Course c = createCourse("cst311", "Security", 3);
+
+        /* Generate Section */
+        Section s = createSection(c,
+                t,
+                111,
+                "99",
+                "b101",
+                "W F 10-11",
+                "ted@csumb.edu");
 
         /* Add Course to Student's Schedule */
         // add a course to the student's schedule in POST request with sectionNo as path var
-        EntityExchangeResult<EnrollmentDTO> enrollmentResponse =  client.post().uri("/enrollments/sections/"+sectionNo)
+        EntityExchangeResult<EnrollmentDTO> enrollmentResponse =  client.post().uri("/enrollments/sections/"+s.getSectionNo())
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -61,7 +95,7 @@ public class StudentScheduleControllerUnitTest {
 
         // check that the new Enrollment exists in the database
         int userId = actualEnrollment.studentId();
-        Enrollment e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(sectionNo, userId);
+        Enrollment e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(s.getSectionNo(), userId);
         assertNotNull(e, "enrollment created was ok but enrollment is not in database");
         assertNull(e.getGrade(), "enrollment created was ok but grade should be null");
 
@@ -74,7 +108,7 @@ public class StudentScheduleControllerUnitTest {
                 .expectStatus().isOk();
 
         // check that the student's enrollment no longer exists in the database
-        e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(sectionNo, userId);
+        e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(s.getSectionNo(), userId);
         assertNull(e, "enrollment was not deleted from database");
 
         // check that the sendMessage to gradebook service was called as expected
@@ -83,17 +117,44 @@ public class StudentScheduleControllerUnitTest {
 
     @Test
     public void createStudentScheduleAlreadyEnrolled() throws Exception {
+
+        // reset databases used in this test
+        enrollmentRepository.deleteAll();
+        sectionRepository.deleteAll();
+        courseRepository.deleteAll();
+        termRepository.deleteAll();
+
         /* Student Login */
         String studentEmail = "sam@csumb.edu";
         String password = "sam2025";
 
         String jwt = loginUser(studentEmail, password);
 
-        int sectionNo = 1; // default section already in database
+        /* Generate Term */
+        Term t = createTerm(1,
+                2026,
+                "Spring",
+                "2025-11-01",
+                "2026-04-30",
+                "2026-04-30",
+                "2026-01-15",
+                "2026-05-17");
+
+        /* Generate Course */
+        Course c = createCourse("cst311", "Security", 3);
+
+        /* Generate Section */
+        Section s = createSection(c,
+                t,
+                111,
+                "99",
+                "b101",
+                "W F 10-11",
+                "ted@csumb.edu");
 
         /* Add Course to Student's Schedule */
         // add a course to the student's schedule in POST request with sectionNo as path var
-        EntityExchangeResult<EnrollmentDTO> enrollmentResponse =  client.post().uri("/enrollments/sections/"+sectionNo)
+        EntityExchangeResult<EnrollmentDTO> enrollmentResponse =  client.post().uri("/enrollments/sections/"+s.getSectionNo())
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -107,25 +168,30 @@ public class StudentScheduleControllerUnitTest {
 
         // check that the new Enrollment exists in the database
         int userId = actualEnrollment.studentId();
-        Enrollment e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(sectionNo, userId);
+        Enrollment e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(s.getSectionNo(), userId);
         assertNotNull(e, "enrollment created was ok but enrollment is not in database");
         assertNull(e.getGrade(), "enrollment created was ok but grade should be null");
 
         /* Attempt to Re-add Course to Student's Schedule */
         // add a course to the student's schedule in POST request with sectionNo as path var
-        client.post().uri("/enrollments/sections/"+sectionNo)
+        client.post().uri("/enrollments/sections/"+s.getSectionNo())
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isBadRequest()
                 .expectBody()
                 .jsonPath("$.errors[?(@=='student already enrolled in course')]").exists();
-
-        clearEnrollment(jwt, e.getEnrollmentId(), sectionNo, userId);
     }
 
     @Test
     public void deleteStudentScheduleNotEnrolled() throws Exception {
+
+        // reset databases used in this test
+        enrollmentRepository.deleteAll();
+        sectionRepository.deleteAll();
+        courseRepository.deleteAll();
+        termRepository.deleteAll();
+
         /* Student Login */
         String studentEmail = "sam@csumb.edu";
         String password = "sam2025";
@@ -134,8 +200,8 @@ public class StudentScheduleControllerUnitTest {
 
         int enrollmentId = 0; // non-existent enrollmentId
 
-        /* Attempt to Re-add Course to Student's Schedule */
-        // add a course to the student's schedule in POST request with sectionNo as path var
+        /* Attempt to add Course to Student's Schedule */
+        // add a course to the student's schedule in POST request with non-existent enrollmentId as path var
         client.delete().uri("/enrollments/"+enrollmentId)
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
@@ -146,6 +212,12 @@ public class StudentScheduleControllerUnitTest {
     @Test
     public void createStudentScheduleInvalidDate() throws Exception {
 
+        // reset databases used in this test
+        enrollmentRepository.deleteAll();
+        sectionRepository.deleteAll();
+        courseRepository.deleteAll();
+        termRepository.deleteAll();
+
         /* Student Login */
         String studentEmail = "sam@csumb.edu";
         String password = "sam2025";
@@ -153,11 +225,31 @@ public class StudentScheduleControllerUnitTest {
         String jwt = loginUser(studentEmail, password);
 
         /* After Add Deadline */
-        int sectionNo = 2; // default section already in database that is past the add deadline
+        /* Generate Term */
+        Term t = createTerm(1,
+                2026,
+                "Spring",
+                "2024-11-01",
+                "2025-04-30",
+                "2026-04-30",
+                "2026-01-15",
+                "2026-05-17");
+
+        /* Generate Course */
+        Course c = createCourse("cst311", "Security", 3);
+
+        /* Generate Section */
+        Section s = createSection(c,
+                t,
+                111,
+                "99",
+                "b101",
+                "W F 10-11",
+                "ted@csumb.edu");
 
         /* Add Course to Student's Schedule */
         // add a course to the student's schedule in POST request with sectionNo as path var
-        client.post().uri("/enrollments/sections/"+sectionNo)
+        client.post().uri("/enrollments/sections/"+s.getSectionNo())
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -166,11 +258,31 @@ public class StudentScheduleControllerUnitTest {
                 .jsonPath("$.errors[?(@=='add deadline passed')]").exists();
 
         /* Before Add Date */
-        sectionNo = 3; // default section already in database that is past the add deadline
+        /* Generate Term */
+        t = createTerm(2,
+                2027,
+                "Spring",
+                "2026-11-01",
+                "2026-04-30",
+                "2026-04-30",
+                "2026-01-15",
+                "2026-05-17");
+
+        /* Generate Course */
+        c = createCourse("cst322", "Security3", 5);
+
+        /* Generate Section */
+        s = createSection(c,
+                t,
+                111,
+                "99",
+                "b101",
+                "W F 10-11",
+                "ted@csumb.edu");
 
         /* Add Course to Student's Schedule */
         // add a course to the student's schedule in POST request with sectionNo as path var
-        client.post().uri("/enrollments/sections/"+sectionNo)
+        client.post().uri("/enrollments/sections/"+s.getSectionNo())
                 .headers(headers -> headers.setBearerAuth(jwt))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -194,18 +306,57 @@ public class StudentScheduleControllerUnitTest {
         return jwt;
     }
 
-    private void clearEnrollment(String jwt, int enrollmentId, int sectionNo, int userId) {
-        // drop a course from the student's schedule in DELETE request with enrollmentId as path var
-        client.delete().uri("/enrollments/"+enrollmentId)
-                .headers(headers -> headers.setBearerAuth(jwt))
-                .exchange()
-                .expectStatus().isOk();
+    // create a term in the database
+    private Term createTerm(int termId,
+                            int year,
+                            String semester,
+                            String addDate,
+                            String addDeadline,
+                            String dropDeadline,
+                            String startDate,
+                            String endDate) {
+        Term t = new Term();
+        t.setTermId(termId);
+        t.setYear(year);
+        t.setSemester(semester);
+        t.setAddDate(Date.valueOf(addDate));
+        t.setAddDeadline(Date.valueOf(addDeadline));
+        t.setDropDeadline(Date.valueOf(dropDeadline));
+        t.setStartDate(Date.valueOf(startDate));
+        t.setEndDate(Date.valueOf(endDate));
+        termRepository.save(t);
+        return t;
+    }
 
-        // check that the student's enrollment no longer exists in the database
-        Enrollment e = enrollmentRepository.findEnrollmentBySectionNoAndStudentId(sectionNo, userId);
-        assertNull(e, "enrollment was not deleted from database");
+    // create a course in the database
+    private Course createCourse(String courseId,
+                                String title,
+                                int credits) {
+        Course c = new Course();
+        c.setCourseId(courseId);
+        c.setTitle(title);
+        c.setCredits(credits);
+        courseRepository.save(c);
+        return c;
+    }
 
-        // check that the sendMessage to gradebook service was called as expected
-        verify(gradebookService, times(1)).sendMessage(eq("deleteEnrollment"), any());
+    // create a section in the database
+    private Section createSection(Course c,
+                                  Term t,
+                                  int sectionId,
+                                  String building,
+                                  String room,
+                                  String times,
+                                  String instructorEmail) {
+        Section s = new Section();
+        s.setCourse(c);
+        s.setTerm(t);
+        s.setSectionId(sectionId);
+        s.setBuilding(building);
+        s.setRoom(room);
+        s.setTimes(times);
+        s.setInstructorEmail(instructorEmail);
+        sectionRepository.save(s);
+        return s;
     }
 }
